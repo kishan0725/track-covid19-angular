9396af7085b84614d782e63603023257a4b3c537
KLAL.C <kishan.lal.c@greenwayhealth.com>
2020-11-11 19:46:37 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { SharedCountryService } from 'src/app/services/shared-country.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  public data;
   title = 'covid-tracker';
+  public darkMode = false;
+  constructor(private _sharedService: SharedCountryService) {
+    this._sharedService.dataSource.subscribe((data) => {
+      this.data = data;
+    });
+  }
+
+  toggleDarkMode = () => {
+    this.darkMode = !this.darkMode;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { CountryPickerComponent } from './components/country-picker/country-picker.component';
+import { CovidTrackerService } from './covid-tracker.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ChartsModule } from 'ng2-charts';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ChartComponent,
+    CountryPickerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatProgressSpinnerModule,
+    ChartsModule
   ],
-  providers: [],
+  providers: [CovidTrackerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
